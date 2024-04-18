4114200bf0d68445cfaea626dccc62accdeededc
T-Letuka <tisetsoletuka@gmail.com>
2024-04-18 19:05:48 +0200

    added responsive nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-scroll";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="nav-bar container ">
       <img src={logo} alt="logo-picture" className="logo" />
-      <ul className="nav-list">
+      <button className="menu-toggle" onClick={toggleMenu}>
+        {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </button>
+      <ul id="nav-list" className={`navbar-menu ${isOpen ? "open" : " "}`}>
         <li>
           <Link to="home" smooth={true} offset={-260} duration={500}>
             Home
